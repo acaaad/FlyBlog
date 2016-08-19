9cfc714645e8ebf11c3de9e6927045c7264c924f
acaaad <auliarshad@gmail.com>
2016-08-20 01:30:57 +0900

    indexjs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit: 3,
+  host: 'localhost:3306',
+  user: 'root',
+  database: 'blogdb',
+  password: '161718'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/show', function(req, res, next){
+  pool.getConnections(function(err, connection) {
+    var sql = "SELECT * from user";
+
+    connection.query(sql, function(err, row){
+      if(err) console.error("err: "+err);
+      console.log("row: "+JSON.stringify(row));
+      res.json(row);
+      connection.release();
+    })
+  })
+})
+
 
 module.exports = router;
